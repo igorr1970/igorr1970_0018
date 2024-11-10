b458002c8126666aeffe40b6411e9d1b6c3070de
Игорь Рябухин <igorr1970@mail.ru>
2024-11-10 22:17:24 +0500

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,23 @@
+def all_variants(text):
+    for i in range(1, len(text) + 1):
+        for j in range(len(text) - i + 1):
+            yield text[j:j + i]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+
+
+def all_variants(text):
+    length = len(text)
+
+    for start in range(length):
+        for end in range(start + 1, length + 1):
+            yield text[start:end]
+
+
+# Пример использования функции
+a = all_variants("abc")
+for i in a:
+    print(i)
